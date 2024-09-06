13b8cc0da2b8b12be8104b6c9432ab88758c5a69
abidkhokhar <dev.abidkk@gmail.com>
2024-09-06 20:18:51 +0500

    hello world

---
+++ 00_hello/hello.java
@@ -0,0 +1,7 @@
+public class hello {
+    public static void main (String[] args){
+        System.out.println("Hello Java");  // line break with println
+        System.out.print("Hello World! "); // no line break : print
+        System.out.println("Hi, Abid Khokhar");  
+    }
+}
