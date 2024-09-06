72a5b38dd94cc87cf4f9c6d61eefe42c55689c41
abidkhokhar <dev.abidkk@gmail.com>
2024-09-06 21:29:30 +0500

    type casting

---
+++ 03_type-casting/tcast.java
@@ -0,0 +1,19 @@
+public class tcast{
+    static public void main(String[] args){
+
+        // integet to float casting
+        int num1 = 2028;
+        float num2 = num1;
+        System.out.println(num1); // 2028
+        System.out.println(num2); // 2028.0
+
+    }
+
+
+    
+}
+
+
+// Types of Casting
+// (1): widening casting is automatic and safe.
+// (2): narrowing casting requires explicit handling and may lead to data loss.
