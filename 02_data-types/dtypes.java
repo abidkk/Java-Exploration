d03372b6d16ab4594c6f25a720d703b570a5df93
abidkhokhar <dev.abidkk@gmail.com>
2024-09-06 20:51:07 +0500

    string

---
+++ 02_data-types/dtypes.java
@@ -0,0 +1,13 @@
+// Data Types
+
+public class dtypes{
+    static public void main(String[] args){
+
+        // 1 String : letters, symbols, number in double quotes
+        String myCourse = "Object Oriented Programming";
+        String mySemester = "2nd";
+        System.out.println(myCourse);
+        System.out.println(mySemester);
+
+    }
+}
