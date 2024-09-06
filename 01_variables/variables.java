c29ba7f5dd0a19634e2dc339a3ff722e71f51b10
abidkhokhar <dev.abidkk@gmail.com>
2024-09-06 20:35:26 +0500

    variables

---
+++ 01_variables/variables.java
@@ -0,0 +1,23 @@
+// Variables are containers to store data
+// OR
+// A variable is a named storage location in memory that can hold a value
+
+public class variables{
+    static public void main(String[] args){
+     
+        // Variables
+        String myName = "Abid Khokhar";
+        String myCountry = "Pakistan";
+        int myNum = 10;
+
+
+        System.out.println(myName); // Abid Khokhar
+        System.out.println(myCountry); // Pakistan
+        System.out.println(myNum); // 10
+        System.out.println(myName + " " + myCountry); // Abid Khokhar Pakistan
+    }
+}
+
+
+
+// Note : here , myName, myCountry and myNum are variables and also known as identifiers
