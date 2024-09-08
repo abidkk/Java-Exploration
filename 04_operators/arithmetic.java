af904c4c253f6cc3cbcae7b3b3528c8fd3286fb4
abidkhokhar <dev.abidkk@gmail.com>
2024-09-08 21:58:53 +0500

    arithmetic ope

---
+++ 04_operators/arithmetic.java
@@ -0,0 +1,42 @@
+public class arithmetic{
+    static public void main(String[] args){
+        
+        //  1. Addition
+        System.out.println(20+30); // 50
+        System.out.println(5.4 + 4.6); // 10.0 
+
+        //  2. Subtraction
+        System.out.println(20-8); // 12
+        System.out.println(30-100); // 70
+
+        //  3. Multiplication
+        System.out.println(6*5); // 30
+        System.out.println(7*3); // 21
+
+
+        //  4. Division
+        System.out.println(20 / 4); // 5
+        System.out.println(30 / 4); // 
+
+        System.out.println(30 / 4); //  7 : reminder will not be returned
+
+        //  5. Modulus : Reminder 
+        System.out.println(12 % 5); // 2
+        System.out.println(20 % 4); // 0
+
+
+        // 6. Increement ++
+        int n1 = 2;
+        ++n1;
+        System.out.println(n1); // 3
+
+
+        // 7. Decrement --
+        int n2 = 5;
+        --n2;
+        System.out.println(n2); // 4
+
+    }
+}
+
+
