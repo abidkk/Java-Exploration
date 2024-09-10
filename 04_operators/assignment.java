4cc0ac9b8c7e8dc046e943c4ab42a47f433a88ec
abidkhokhar <dev.abidkk@gmail.com>
2024-09-10 08:52:28 +0500

    assignment

---
+++ 04_operators/assignment.java
@@ -0,0 +1,36 @@
+public class assignment{
+    static public void main(String[] args){
+       
+        // 1. assign opertator (=)
+        int v1 = 50;
+        System.err.println(v1); // 50
+
+
+        // 2. add & assign operator (+=)
+        v1+=30;
+        System.err.println(v1); // 80
+
+        // 3. subtract & assign
+        v1-=10;
+        System.err.println(v1); // 70
+
+        // 4. multiply & assign
+        v1*=2;
+        System.err.println(v1); // 140
+
+
+        // 5. divide & assign
+        v1/=10;
+        System.err.println(v1); // 14
+
+        
+        // 6. modulus & assign 
+        v1%=5;
+        System.err.println(v1); // 4
+
+        
+        
+        // 7. 
+    }
+
+}
